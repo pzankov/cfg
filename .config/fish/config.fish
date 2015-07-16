d3f6f372b233eacb3f3ca0e3934176ceceebfd77
Petr Zankov <pzankov@yandex.ru>
2015-07-16 14:50:18 +0200

    fish: alias to find files except build folders

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
 alias time "time --portability"
 alias s "git status"
 alias d "git diff"
+alias ff "find . -not \( -path './build*/*' -prune \) -not \( -path './.*/*' -prune \) -not \( -path '*/venv/*' -prune \) -type f"
