af273de06004361f19fe03cdcdb787f9918a680a
Petr Zankov <peter@cyberhaven.io>
2015-09-07 14:40:05 +0200

    prune *.pyc files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 alias time "time --portability"
 alias s "git status"
 alias d "git diff"
-alias ff "find . -not \( -path './build*/*' -prune \) -not \( -path './.*/*' -prune \) -not \( -path '*/venv/*' -prune \) -type f"
+alias ff "find . -not \( -path './build*/*' -prune \) -not \( -path './.*/*' -prune \) -not \( -path '*/venv/*' -prune \) -not \( -name '*.pyc' -prune \) -type f"
